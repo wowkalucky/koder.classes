8a651b3bc1e8ea80bd4cdcacb2eef24bc43cfc71
wowkalucky <wowkalucky@gmail.com>
2014-03-28 11:32:16 +0200

    iter_lines

---
+++ p2_iter_lines.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+
+
+def iter_lines(fd):
+    """
+    Получает имя файла итерирует по строкам.
+    Для чтения можно использовать только fd.read(1)
+    """
+    line = ''
+    while True:
+        s = fd.read(1)
+        if s == "":
+            break
+        line += s
+        if s == '\n':
+            yield line
+            line = ''
+    yield line
+
+
+if __name__ == '__main__':
+    for line in iter_lines(open(sys.argv[0])):
+        print repr(line)
