3c72d99c6ddfc90cabd47524712736d7487d7975
wowkalucky <wowkalucky@gmail.com>
2014-04-06 12:49:47 +0300

    get_ints

---
+++ p2_get_ints.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+import sys
+from p2_drop_empty import drop_empty
+from p2_iter_lines import iter_lines
+from p2_split_items import split_items
+from p2_strip_spaces import strip_spaces
+
+teststring = """
+    123 asfd afsd df11
+    asdf fdfg 2.0 asdf
+    ad.sdf 6sd.f9sdf
+    asdf 45 asdfasd 45fd
+    2asdfa fds8"""
+
+def get_ints(iter):
+    """ Возращает из входного потока только целые """
+    for item in iter:
+        if isinstance(item, int):
+            yield item
+
+
+if __name__ == '__main__':
+    gen1 = iter_lines(open(sys.argv[0]))
+    gen2 = strip_spaces(gen1)
+    gen3 = drop_empty(gen2)
+    gen4 = split_items(gen3)
+    gen5 = get_ints(gen4)
+    for line in gen5:
+        print repr(line)
