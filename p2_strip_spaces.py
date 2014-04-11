a5434189a865e79079c6af55b80227068c5a9626
wowkalucky <wowkalucky@gmail.com>
2014-04-12 00:22:32 +0300

    strip_spaces

--- p2_strip_spaces.py
+++ p2_strip_spaces.py
@@ -12,6 +12,10 @@ def strip_spaces(iter):
         yield iter.next().strip()
 
 
+with open("test_file.txt", "r") as fd:
+    assert list(strip_spaces(iter_lines(fd))) == ['1 2 3 3.45 abra_cadabra', '', '12']
+
+
 if __name__ == '__main__':
     gen1 = iter_lines(open(sys.argv[0]))
     gen2 = strip_spaces(gen1)
