2896ab4447b7e94764b71413af043e6428b3097b
wowkalucky <wowkalucky@gmail.com>
2014-04-12 00:39:49 +0300

    get_ints

--- p2_get_ints.py
+++ p2_get_ints.py
@@ -1,10 +1,11 @@
 # -*- encoding: utf-8 -*-
 
 import sys
-from p2_drop_empty import drop_empty
 from p2_iter_lines import iter_lines
-from p2_split_items import split_items
 from p2_strip_spaces import strip_spaces
+from p2_drop_empty import drop_empty
+from p2_split_items import split_items
+
 
 teststring = """
     123 asfd afsd df11
@@ -13,11 +14,16 @@ teststring = """
     asdf 45 asdfasd 45fd
     2asdfa fds8"""
 
+
 def get_ints(iter):
     """ Возращает из входного потока только целые """
     for item in iter:
         if isinstance(item, int):
             yield item
+
+
+with open("test_file.txt", "r") as fd:
+    assert list(get_ints(split_items(drop_empty(strip_spaces(iter_lines(fd))))))  == [1, 2, 3, 12]
 
 
 if __name__ == '__main__':
