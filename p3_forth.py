2071e667d9199c7b50af1f0d02fd0ff6e7c119dd
wowkalucky <wowkalucky@gmail.com>
2014-04-14 15:09:10 +0300

    forth

--- p3_forth.py
+++ p3_forth.py
@@ -1,4 +1,4 @@
-# -*- enc0ding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 
 """
 интерпретатор подмножества языка forth
@@ -38,5 +38,80 @@ print
 строку на языке forth и исполняющую ее. По умолчанию из main вызывать
 eval_forth("example.frt")
 """
+import string
 
-__author__ = 'wowkalucky'
+
+STACK = []
+
+def run_command(cmd_name, *args):
+    def put(*arg):
+        STACK.append(arg[0])
+
+    locals()[cmd_name](args)
+
+
+
+def validate(parts):
+    commands = {'put': 1, 'pop': 0, 'add': 0, 'sub': 0, 'print': 0}
+    if parts and parts[0] in commands:
+        if len(parts[1:]) != commands[parts[0]]:
+            raise TypeError, '"%s" takes exactly %d arguments (%d given)' % (
+                parts[0], commands[parts[0]], len(parts[1:]))
+        else:
+            return True
+    # etc
+    return False
+
+
+def parser(strng):
+    def argparse(arg):
+        if arg and arg[0] in string.digits:
+            try:
+                arg = int(arg)
+            except:
+                try:
+                    arg = float(arg)
+                except:
+                    pass
+        elif arg and arg[0] not in ["'", '"']:
+            raise TypeError, 'Arguments must be int, float or string (in single or double quotes)'
+        return arg
+
+    strng = strng.rstrip('\n')
+    parsed = {}
+    if strng[0] == "#":
+        parsed['type'] = 'comment'
+        parsed['value'] = strng
+        parsed['args'] = None
+        return parsed
+
+    parts = strng.split(" ")
+    if validate(parts):
+        parsed['type'] = 'command'
+        parsed['value'] = parts[0]
+        parsed['args'] = map(argparse, parts[1:])
+        return parsed
+
+
+def proc_line(line):
+    parsed = parser(line)
+    if parsed and parsed['type'] == 'command':
+        command = parsed['value']
+        args = parsed['args']
+        print command, args
+        print STACK
+        run_command(command, args)
+        print STACK
+
+
+def eval_forth(filename):
+    with open(filename, 'r') as fd:
+        while True:
+            line = fd.readline()
+            if line == "":
+                break
+            proc_line(line)
+
+
+if __name__ == '__main__':
+    eval_forth("example.frt")
