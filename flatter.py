702077e4b486c8c822cb92e831bccb991d593bf0
wowkalucky <wowkalucky@gmail.com>
2014-04-30 10:56:47 +0300

    Added dumbs

---
+++ flatter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Условие
+"""
+
+__author__ = 'wowkalucky'
+
+
+def flatter(list_or_gen):
+    """
+    Gets list or generator
+    -> flatten list
+    """
+    pass
